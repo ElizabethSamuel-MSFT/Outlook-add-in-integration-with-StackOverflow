e6c01414c5f24cd23e9e5b1e97c60ee2e7bea67c
Alex Jerabek <aljerabe@microsoft.com>
2018-07-25 09:52:13 -0700

    Adding basic message read functionality

--- TakeItOnline/TakeItOnlineWeb/MessageRead.js
+++ TakeItOnline/TakeItOnlineWeb/MessageRead.js
@@ -1,90 +1,132 @@
-﻿(function () {
-  "use strict";
-
-  var messageBanner;
-
-  // The Office initialize function must be run each time a new page is loaded.
-  Office.initialize = function (reason) {
-    $(document).ready(function () {
-      var element = document.querySelector('.ms-MessageBanner');
-      messageBanner = new fabric.MessageBanner(element);
-      messageBanner.hideBanner();
-      loadProps();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-
-  // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
-  function buildAttachmentsString(attachments) {
-    if (attachments && attachments.length > 0) {
-      var returnString = "";
-      
-      for (var i = 0; i < attachments.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
-        }
-        returnString = returnString + attachments[i].name;
-      }
-
-      return returnString;
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-
-    return "None";
-  }
-
-  // Format an EmailAddressDetails object as
-  // GivenName Surname <emailaddress>
-  function buildEmailAddressString(address) {
-    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
-  }
-
-  // Take an array of EmailAddressDetails objects and
-  // build a list of formatted strings, separated by a line-break
-  function buildEmailAddressesString(addresses) {
-    if (addresses && addresses.length > 0) {
-      var returnString = "";
-
-      for (var i = 0; i < addresses.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
+};
+(function () {
+    "use strict";
+    var messageBanner;
+    // The Office initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+            $("#getMessage").click(function () { return tryCatch(loadMessage); });
+        });
+    };
+    // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
+    function buildAttachmentsString(attachments) {
+        if (attachments && attachments.length > 0) {
+            var returnString = "";
+            for (var i = 0; i < attachments.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + attachments[i].name;
+            }
+            return returnString;
         }
-        returnString = returnString + buildEmailAddressString(addresses[i]);
-      }
-
-      return returnString;
+        return "None";
     }
-
-    return "None";
-  }
-
-  // Load properties from the Item base object, then load the
-  // message-specific properties.
-  function loadProps() {
-    var item = Office.context.mailbox.item;
-
-    $('#dateTimeCreated').text(item.dateTimeCreated.toLocaleString());
-    $('#dateTimeModified').text(item.dateTimeModified.toLocaleString());
-    $('#itemClass').text(item.itemClass);
-    $('#itemId').text(item.itemId);
-    $('#itemType').text(item.itemType);
-
-    $('#message-props').show();
-
-    $('#attachments').html(buildAttachmentsString(item.attachments));
-    $('#cc').html(buildEmailAddressesString(item.cc));
-    $('#conversationId').text(item.conversationId);
-    $('#from').html(buildEmailAddressString(item.from));
-    $('#internetMessageId').text(item.internetMessageId);
-    $('#normalizedSubject').text(item.normalizedSubject);
-    $('#sender').html(buildEmailAddressString(item.sender));
-    $('#subject').text(item.subject);
-    $('#to').html(buildEmailAddressesString(item.to));
-  }
-
-  // Helper function for displaying notifications
-  function showNotification(header, content) {
-    $("#notificationHeader").text(header);
-    $("#notificationBody").text(content);
-    messageBanner.showBanner();
-    messageBanner.toggleExpansion();
-  }
+    // Format an EmailAddressDetails object as
+    // GivenName Surname <emailaddress>
+    function buildEmailAddressString(address) {
+        return address.displayName + " &lt;" + address.emailAddress + "&gt;";
+    }
+    // Take an array of EmailAddressDetails objects and
+    // build a list of formatted strings, separated by a line-break
+    function buildEmailAddressesString(addresses) {
+        if (addresses && addresses.length > 0) {
+            var returnString = "";
+            for (var i = 0; i < addresses.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + buildEmailAddressString(addresses[i]);
+            }
+            return returnString;
+        }
+        return "None";
+    }
+    // Load properties from the Item base object, then load the
+    // message-specific properties.
+    function loadMessage() {
+        return __awaiter(this, void 0, void 0, function () {
+            var item, readMessageItem;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        $('#debug').text("here");
+                        item = Office.context.mailbox.item;
+                        readMessageItem = item;
+                        return [4 /*yield*/, readMessageItem.body.getAsync(Office.CoercionType.Html, function (result) {
+                                $('#debug').text("now here");
+                                $('#message').text(result.value);
+                            })];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    }
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+    /** Default helper for invoking an action and handling errors. */
+    function tryCatch(callback) {
+        return __awaiter(this, void 0, void 0, function () {
+            var error_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 2, , 3]);
+                        return [4 /*yield*/, callback()];
+                    case 1:
+                        _a.sent();
+                        return [3 /*break*/, 3];
+                    case 2:
+                        error_1 = _a.sent();
+                        console.log(error_1);
+                        return [3 /*break*/, 3];
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
+    }
 })();
---
+++ TakeItOnline/TakeItOnlineWeb/MessageRead.ts
@@ -0,0 +1,87 @@
+﻿declare var fabric: any;
+
+(function () {
+    "use strict";
+    var messageBanner;
+
+    // The Office initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+            $("#getMessage").click(() => tryCatch(loadMessage));
+        });
+    };
+
+    // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
+    function buildAttachmentsString(attachments) {
+        if (attachments && attachments.length > 0) {
+            var returnString = "";
+
+            for (var i = 0; i < attachments.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + attachments[i].name;
+            }
+
+            return returnString;
+        }
+
+        return "None";
+    }
+
+    // Format an EmailAddressDetails object as
+    // GivenName Surname <emailaddress>
+    function buildEmailAddressString(address) {
+        return address.displayName + " &lt;" + address.emailAddress + "&gt;";
+    }
+
+    // Take an array of EmailAddressDetails objects and
+    // build a list of formatted strings, separated by a line-break
+    function buildEmailAddressesString(addresses) {
+        if (addresses && addresses.length > 0) {
+            var returnString = "";
+
+            for (var i = 0; i < addresses.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + buildEmailAddressString(addresses[i]);
+            }
+
+            return returnString;
+        }
+
+        return "None";
+    }
+
+    // Load properties from the Item base object, then load the
+    // message-specific properties.
+    async function loadMessage() {
+        var item = Office.context.mailbox.item;
+        var readMessageItem = item as Office.MessageRead;
+        await readMessageItem.body.getAsync(Office.CoercionType.Text, function (result) {
+            $('#message').text(result.value);
+        });
+    }
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+
+    /** Default helper for invoking an action and handling errors. */
+    async function tryCatch(callback) {
+        try {
+            await callback();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+})();
